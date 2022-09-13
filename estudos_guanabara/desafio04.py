0b0d7df22ec0c375343f87b417c1f1a23244f0d5
Samuel L. Nogueira <linharescontatotecnico@gmail.com>
2022-09-13 10:22:20 -0300

    Update desafio04.py

--- estudos_guanabara/desafio04.py
+++ estudos_guanabara/desafio04.py
@@ -4,4 +4,7 @@ print(f"{algo} é do tipo: {type(algo)}")
 print(f"{algo} é numerico? {algo.isnumeric()}")
 print(f"{algo} é alfabetico? {algo.isalpha()}")
 print(f"{algo} é alfanumerico? {algo.isalnum()}")
+print(f"{algo} só tem espaços? {algo.isspace()}")
 print(f"{algo} possui todas as letras maiusculas? {algo.isupper()}")
+print(f"{algo} possui todas as letras minusculas? {algo.islower()}")
+print(f"{algo} está capitalizada? {algo.istitle()}") #capitalizada é a mistura de maiuscula e minuscula
