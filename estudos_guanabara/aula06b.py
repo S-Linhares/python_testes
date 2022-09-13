69c92563bcf63c762b70a5e9f3899e3d35106b71
Samuel L. Nogueira <linharescontatotecnico@gmail.com>
2022-09-13 09:57:07 -0300

    Create aula06b.py

---
+++ estudos_guanabara/aula06b.py
@@ -0,0 +1,8 @@
+n = input('Digite um numero: ')
+m = bool(input('Digite outro numero: '))
+print(n)
+print(m)
+print(n.isnumeric()) #fala se o que foi inserido é numerico
+print(n.isalpha()) #fala se o que foi inserido é alfabetico
+print(n.isalnum()) #fala se o que foi inserido é alfanumerico
+print(n.isupper()) #fala se o que foi inserido está escrito apenas com letras maiusculas
