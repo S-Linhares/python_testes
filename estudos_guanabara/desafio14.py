2bbe5d00c9668e1537d1075d41b854a4a18af56f
Samuel L. Nogueira <linharescontatotecnico@gmail.com>
2022-09-14 10:47:14 -0300

    desafios

---
+++ estudos_guanabara/desafio14.py
@@ -0,0 +1,3 @@
+temp = float(input('Informe a temperatura em ºC: '))
+fah = (temp * 1.8)+32
+print(f'A temperatura em Fahrenheit é: {fah:.1f}ºF')
---
+++ estudos_guanabara/desafio15.py
@@ -0,0 +1,4 @@
+km = float(input('Informe quantos km rodou: '))
+dias = int(input('Informe quantos dias foi alugado: '))
+valor = (60*dias)+(km*0.15)
+print(f'O valor total a pagar é: {valor:.2f}')
