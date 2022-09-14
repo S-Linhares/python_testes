c9ad3bdfe8301ff80a456d3227130a9c5a774b37
Samuel L. Nogueira <linharescontatotecnico@gmail.com>
2022-09-14 09:44:52 -0300

    desafios

---
+++ estudos_guanabara/desafio05.py
@@ -0,0 +1,2 @@
+num = int(input('Insira um numero: '))
+print(f'O antecessor desse numero é: {num-1}\nO sucessor desse numero é: {num+1}\n')
---
+++ estudos_guanabara/desafio06.py
@@ -0,0 +1,3 @@
+num = int(input('Insira um numero: '))
+print(f'O dobro desse numero é: {num*2}\nO triplo desse numero é: {num*3}\n'
+      f'A raiz quadrada desse numero é: {num**(1/2)}\n')
