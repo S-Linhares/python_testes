8a3bf28edf30fc8cd6afe05a230ce2f0462ee75f
Samuel L. Nogueira <linharescontatotecnico@gmail.com>
2022-09-19 08:36:40 -0300

    Update desafio32.py

--- estudos_guanabara/desafio32.py
+++ estudos_guanabara/desafio32.py
@@ -1,4 +1,7 @@
+import datetime
 ano = int(input('Digite o ano que deseja: '))
+if ano == 0:
+    ano = datetime.date.today().year
 if ((ano % 4 == 0) and (ano % 100 != 0)) or ((ano % 4 == 0) and (ano % 100 == 0) and (ano % 400 == 0)):
     print(f'O ano {ano} é bissexto!')
 else:
