303700a03910ff1f669d926ff5d9cd8998880f36
Samuel L. Nogueira <linharescontatotecnico@gmail.com>
2022-09-21 16:11:14 -0300

    Update desafio37.py

--- estudos_guanabara/desafio37.py
+++ estudos_guanabara/desafio37.py
@@ -5,12 +5,12 @@ conversao = int(input('\nInforme para qual base deseja converter o numero:'
                       '\n3 - hexadecimal\n'))
 if conversao == 1:
     numero = bin(numero)
-    print(f'\nNúmero convertido para binario: {numero}')
+    print(f'\nNúmero convertido para binario: {numero[2:]}')
 elif conversao == 3:
     numero = hex(numero)
-    print(f'\nNumero convertido para hexadecimal: {numero}')
+    print(f'\nNumero convertido para hexadecimal: {numero[2:]}')
 elif conversao == 2:
     numero = oct(numero)
-    print(f'\nNúmero convertido para octal: {numero}')
+    print(f'\nNúmero convertido para octal: {numero[2:]}')
 else:
     print('Opção inválida!!!!')
