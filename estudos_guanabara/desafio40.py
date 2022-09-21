7acebd936064e51410f5b0dfb4d1a0e2f0320584
Samuel L. Nogueira <linharescontatotecnico@gmail.com>
2022-09-21 16:13:27 -0300

    Update desafio40.py

--- estudos_guanabara/desafio40.py
+++ estudos_guanabara/desafio40.py
@@ -2,8 +2,8 @@ n1 = float(input('Informe a primeira nota: '))
 n2 = float(input('Informe a segunda nota: '))
 media = (n1+n2)/2
 if media >= 7.0:
-    print(f'Aprovado! Com média de {media}')
+    print(f'Aprovado! Com média de {media:.2f}')
 elif media < 5.0:
-    print(f'Reprovado! Com média de {media}')
+    print(f'Reprovado! Com média de {media:.2f}')
 else:
-    print(f'Recuperação! Com média de {media}')
+    print(f'Recuperação! Com média de {media:.2f}')
