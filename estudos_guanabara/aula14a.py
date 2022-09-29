73d8781a6dcaecedef4bb162f933942114e4c51e
Samuel L. Nogueira <linharescontatotecnico@gmail.com>
2022-09-29 17:32:57 -0300

    aulas

--- estudos_guanabara/aula14a.py
+++ estudos_guanabara/aula14a.py
@@ -1,4 +1,13 @@
 c = 1
+n = 1
+r = 's'
 while c < 10:
     print(c)
     c += 1
+while n != 0:
+    n = int(input('Digite um valor (DIGITE 0 PARA SAIR): '))
+print('Fim')
+while r == 's':
+    n = int(input('Digite um valor: '))
+    r = str(input('Quer continuar? [S/N] ')).lower()
+print('Fim')
