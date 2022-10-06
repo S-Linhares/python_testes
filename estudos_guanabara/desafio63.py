353303c3aa7357174165c16ea49877a379c7d2b2
Samuel L. Nogueira <linharescontatotecnico@gmail.com>
2022-10-06 01:17:50 -0300

    desafios

--- estudos_guanabara/desafio63.py
+++ estudos_guanabara/desafio63.py
@@ -2,10 +2,13 @@ num = int(input('Informe um numero n: '))
 fibo = 0
 cont = 1
 ant = 0
-while cont <= num:
+aux = 0
+while cont < num:
+    aux = fibo
     fibo = fibo + ant
+    ant = aux
     print(f'{fibo}')
-    ant = fibo
     if fibo == 0:
         fibo += 1
+        print(f'{fibo}')
     cont += 1
