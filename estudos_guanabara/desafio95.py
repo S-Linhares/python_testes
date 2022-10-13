12a26241500536574da6facd86f0f647c781dc15
Samuel L. Nogueira <linharescontatotecnico@gmail.com>
2022-10-13 12:05:01 -0300

    Update desafio95.py

--- estudos_guanabara/desafio95.py
+++ estudos_guanabara/desafio95.py
@@ -1,12 +1,19 @@
 jogador = {'nome': '', 'gols': [], 'total': 0, 'partidas': 0}
+gols = list()
 jogadores = list()
+
 while True:
     jogador['nome'] = str(input('Informe o nome do jogador: '))
     jogador['partidas'] = int(input(f'Informe quantas partidas {jogador["nome"]} jogou: '))
+
     for i in range(0, jogador['partidas']):
-        jogador['gols'].append(int(input(f'Quantos Gols ele fez na partida {i+1}: ')))
-        jogador['total'] += jogador['gols'][i]
+        gols.append(int(input(f'Quantos Gols ele fez na partida {i+1}: ')))
+        jogador['total'] += gols[i]
+
+    jogador['gols'] = gols[:]
     jogadores.append(jogador.copy())
+    gols.clear()
+
     opcao = str(input('Deseja continuar [S/N]?').upper())
     if opcao == 'N':
         break
@@ -14,10 +21,13 @@ while True:
         print('Opção inválida. Digite uma opção válida!!')
         opcao = str(input('Deseja continuar [S/N]?').upper())
 print(60*'=')
+
 for pos, valor in enumerate(jogadores):
     print(f'{pos:<3}', end='')
+
     for k, v in valor.items():
-        print(f'{" ":>2}{v}', end='')
+        print(f'{str(v):<15}', end='')
+
     print('\n', end='')
 #print(f'O nome do jogador é: {jogador["nome"]}')
 #print(f'Os gols, respectivamente, em cada partida foram: {jogador["gols"]}')
